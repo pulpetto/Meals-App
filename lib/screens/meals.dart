3a7eadda29530c1513cbcfdce5020b5e8728b212
pulpetto <116948697+pulpetto@users.noreply.github.com>
2025-05-28 11:07:39 +0200

    display meals for selected category

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/models/meal.dart';
 
 class MealsScreen extends StatelessWidget {
-  const MealsScreen({super.key});
+  const MealsScreen({
+    super.key,
+    required this.title,
+    required this.meals,
+  });
+
+  final String title;
+  final List<Meal> meals;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: meals.isNotEmpty
+          ? ListView.builder(
+              itemCount: meals.length,
+              itemBuilder: (ctx, index) => Text(meals[index].title),
+            )
+          : Text('No meals'),
+    );
   }
 }
