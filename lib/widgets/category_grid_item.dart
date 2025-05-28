fad647091e32164876afa72a7019f1bdbe1caba2
pulpetto <116948697+pulpetto@users.noreply.github.com>
2025-05-28 08:45:38 +0200

    add styling

--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -8,6 +8,23 @@ class CategoryGridItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        gradient: LinearGradient(
+          colors: [
+            category.color.withValues(alpha: 0.55),
+            category.color.withValues(alpha: 0.9),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: Text(
+        category.title,
+        style: Theme.of(context).textTheme.titleLarge!.copyWith(),
+      ),
+    );
   }
 }
