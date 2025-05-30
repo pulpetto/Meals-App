022313f091307b6c2f34f44b11878b213248feec
pulpetto <116948697+pulpetto@users.noreply.github.com>
2025-05-30 15:07:26 +0200

    add filters

---
+++ lib/screens/filters.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+enum Filter {
+  glutenFree,
+  lactoseFree,
+  vegetarian,
+  vegan,
+}
+
+class FiltersScreen extends StatefulWidget {
+  const FiltersScreen({super.key});
+
+  @override
+  State<FiltersScreen> createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  var _glutenFreeFilterSet = false;
+  var _lactoseFreeFilterSet = false;
+  var _vegetarianFilterSet = false;
+  var _veganFilterSet = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your filters'),
+      ),
+      body: PopScope(
+        canPop: false,
+        onPopInvokedWithResult: (
+          bool didPop,
+          dynamic result,
+        ) {
+          if (didPop) return;
+
+          Navigator.of(context).pop(
+            {
+              Filter.glutenFree: _glutenFreeFilterSet,
+              Filter.lactoseFree: _lactoseFreeFilterSet,
+              Filter.vegetarian: _vegetarianFilterSet,
+              Filter.vegan: _veganFilterSet,
+            },
+          );
+        },
+        child: Column(
+          children: [
+            SwitchListTile(
+              value: _glutenFreeFilterSet,
+              onChanged: (isChecked) {
+                setState(() {
+                  _glutenFreeFilterSet = isChecked;
+                });
+              },
+              title: Text('Glutane free'),
+              subtitle: Text('Only include glutane free meals'),
+              activeColor: const Color.fromARGB(12, 2, 241, 18),
+              contentPadding: EdgeInsets.only(left: 34, right: 22),
+            ),
+            SwitchListTile(
+              value: _lactoseFreeFilterSet,
+              onChanged: (isChecked) {
+                setState(() {
+                  _lactoseFreeFilterSet = isChecked;
+                });
+              },
+              title: Text('Lactose free'),
+              subtitle: Text('Only include lactose free meals'),
+              activeColor: const Color.fromARGB(12, 2, 241, 18),
+              contentPadding: EdgeInsets.only(left: 34, right: 22),
+            ),
+            SwitchListTile(
+              value: _vegetarianFilterSet,
+              onChanged: (isChecked) {
+                setState(() {
+                  _vegetarianFilterSet = isChecked;
+                });
+              },
+              title: Text('Vegetarian free'),
+              subtitle: Text('Only include vegetarian meals'),
+              activeColor: const Color.fromARGB(12, 2, 241, 18),
+              contentPadding: EdgeInsets.only(left: 34, right: 22),
+            ),
+            SwitchListTile(
+              value: _veganFilterSet,
+              onChanged: (isChecked) {
+                setState(() {
+                  _veganFilterSet = isChecked;
+                });
+              },
+              title: Text('Vegan free'),
+              subtitle: Text('Only include vegan meals'),
+              activeColor: const Color.fromARGB(12, 2, 241, 18),
+              contentPadding: EdgeInsets.only(left: 34, right: 22),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
